2b17f0c4a4426a94875af6f39563e258bc2385c1
ZekeMarshall <60969328+ZekeMarshall@users.noreply.github.com>
2020-05-29 13:22:08 +0100

    Renaming plots to describe their contents.

--- Data/ValuableLightGraphCode.R
+++ Data/ValuableLightGraphCode.R
@@ -47,48 +47,48 @@ melanopsin <- ggplot(data = Luminosity_functions, aes(x= Wavelength, y=Melanopsi
 
 # Plotting the spectral power distribution of the INC Globe A19 lamp, and associated valuable portions of the spectrum
 
-plot_a <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
+plot_inc <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
   geom_area(fill = "blue") +
   scale_y_continuous(limits = c(0, 0.125)) +
   xy_theme(base_size = 8) +
   ggtitle("INC Globe A19 SPD")
 
-plot_b <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
+plot_inc_p <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
   geom_area(fill = "blue") +
   geom_area(data = INC_Globe_A19_p, mapping = aes(x=Wavelength, y=PWeightedRadiativeFlux), fill = "red") +
   scale_y_continuous(limits = c(0, 0.125)) +
   xy_theme(base_size = 8) +
   ggtitle("P-Weighted INC Globe A19 SPD")
 
-plot_c <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
+plot_inc_u <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
   geom_area(fill = "blue") + 
   geom_area(data = INC_Globe_A19_u, mapping = aes(x=Wavelength, y=UWeightedRadiativeFlux), fill = "red") +
   scale_y_continuous(limits = c(0, 0.125)) +
   xy_theme(base_size = 8) +
   ggtitle("U-Weighted INC Globe A19 SPD")
 
-plot_d <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
+plot_inc_v <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
   geom_area(fill = "blue") + 
   geom_area(data = INC_Globe_A19_v, mapping = aes(x=Wavelength, y=VWeightedRadiativeFlux), fill = "red") +
   scale_y_continuous(limits = c(0, 0.125)) +
   xy_theme(base_size = 8) +
   ggtitle("V-Weighted INC Globe A19 SPD")
 
-plot_e <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=RadiativeFlux)) + ###
+plot_inc_ca <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=RadiativeFlux)) + ###
   geom_area(fill = "blue") + 
   geom_area(data = INC_Globe_A19_ca, mapping = aes(x=Wavelength, y=Chlorophyll_aWeightedRadiativeFlux), fill = "red") +
   scale_y_continuous(limits = c(0, 0.125)) +
   xy_theme(base_size = 8) +
   ggtitle("Ca-Weighted INC Globe A19 SPD")
 
-plot_f <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
+plot_inc_cb <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
   geom_area(fill = "blue") + 
   geom_area(data = INC_Globe_A19_cb, mapping = aes(x=Wavelength, y=Chlorophyll_bWeightedRadiativeFlux), fill = "red") +
   scale_y_continuous(limits = c(0, 0.125)) +
   xy_theme(base_size = 8) +
   ggtitle("Cb-Weighted INC Globe A19 SPD")
 
-plot_g <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
+plot_inc_m <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
   geom_area(fill = "blue") + 
   geom_area(data = INC_Globe_A19_m, mapping = aes(x=Wavelength, y=MelanopsinWeightedRadiativeFlux), fill = "red") +
   scale_y_continuous(limits = c(0, 0.125)) +
@@ -104,55 +104,55 @@ plot_inc_ph <- ggplot(data = INC_Globe_A19, mapping = aes(x=Wavelength, y=Radiat
 
 # Plotting the spectral power distribution of the LED Phillips BR30 lamp, and associated valuable portions of the spectrum
 
-plot_h <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
+plot_led <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
   geom_area(fill = "blue") + 
   scale_y_continuous(limits = c(0, 0.125)) +
   xy_theme(base_size = 8) +
   ggtitle("LED Phillips BR30 SPD")
 
-plot_i <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
+plot_led_p <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
   geom_area(fill = "blue") + 
   geom_area(data = LED_Phillips_BR30_p, mapping = aes(x=Wavelength, y=PWeightedRadiativeFlux), fill = "red") +
   scale_y_continuous(limits = c(0, 0.125)) +
   xy_theme(base_size = 8) +
   ggtitle("P-Weighted LED Phillips BR30 SPD")
 
-plot_j <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
+plot_led_u <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
   geom_area(fill = "blue") + 
   geom_area(data = LED_Phillips_BR30_u, mapping = aes(x=Wavelength, y=UWeightedRadiativeFlux), fill = "red") +
   scale_y_continuous(limits = c(0, 0.125)) +
   xy_theme(base_size = 8) +
   ggtitle("U-Weighted LED Phillips BR30 SPD")
 
-plot_k <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
+plot_led_v <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
   geom_area(fill = "blue") + 
   geom_area(data = LED_Phillips_BR30_v, mapping = aes(x=Wavelength, y=VWeightedRadiativeFlux), fill = "red") +
   scale_y_continuous(limits = c(0, 0.125)) +
   xy_theme(base_size = 8) +
   ggtitle("V-Weighted LED Phillips BR30 SPD")
 
-plot_l <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
+plot_led_ca <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
   geom_area(fill = "blue") + 
   geom_area(data = LED_Phillips_BR30_ca, mapping = aes(x=Wavelength, y=Chlorophyll_aWeightedRadiativeFlux), fill = "red") +
   scale_y_continuous(limits = c(0, 0.125)) +
   xy_theme(base_size = 8) +
   ggtitle("Ca-Weighted LED Phillips BR30 SPD")
 
-plot_m <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
+plot_led_cb <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
   geom_area(fill = "blue") + 
   geom_area(data = LED_Phillips_BR30_cb, mapping = aes(x=Wavelength, y=Chlorophyll_bWeightedRadiativeFlux), fill = "red") +
   scale_y_continuous(limits = c(0, 0.125)) +
   xy_theme(base_size = 8) +
   ggtitle("Cb-Weighted LED Phillips BR30 SPD")
 
-plot_n <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
+plot_led_m <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
   geom_area(fill = "blue") + 
   geom_area(data = LED_Phillips_BR30_m, mapping = aes(x=Wavelength, y=MelanopsinWeightedRadiativeFlux), fill = "red") +
   scale_y_continuous(limits = c(0, 0.125)) +
   xy_theme(base_size = 8) +
   ggtitle("M-Weighted LED Phillips BR30 SPD")
 
-plot_LED_ph <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
+plot_led_ph <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=RadiativeFlux)) + 
   geom_area(fill = "blue") + 
   geom_area(data = LED_Phillips_BR30_ph, mapping = aes(x=Wavelength, y=PhotosynthWeightedRadiativeFlux), fill = "red") +
   scale_y_continuous(limits = c(0, 0.125)) +
@@ -161,9 +161,5 @@ plot_LED_ph <- ggplot(data = LED_Phillips_BR30, mapping = aes(x=Wavelength, y=Ra
 
 # Compiling the individual plots into a composite graph
 
-plot_grid(nfunction, plot_a, plot_h, vspectrum, plot_d, plot_k, pfunction, plot_b, plot_i, melanopsin, plot_g, plot_n, ufunction, plot_c, plot_j, photosyn, plot_inc_ph, plot_LED_ph, ncol = 3, labels = "AUTO")
+plot_grid(nfunction, plot_inc, plot_led, vspectrum, plot_inc_v, plot_led_v, pfunction, plot_inc_p, plot_led_p, melanopsin, plot_inc_m, plot_led_m, ufunction, plot_inc_u, plot_led_u, photosyn, plot_inc_ph, plot_led_ph, ncol = 3, labels = "AUTO")
 
-
-# scale_fill_manual(values = colorRampPalette(brewer.pal(11, "Spectral"))(1253)) +
-# scale_fill_manual(values = "spectral")
-# scale_color_gradient(low = 'greenyellow', high = 'forestgreen') +
