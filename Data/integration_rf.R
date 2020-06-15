46a9b7353468095d22ce517f8ce0287fa75bacbf
manolo <manolo@debian-manolo.localdomain>
2020-06-15 11:14:49 +0200

    Script integration_rf calculates the integral of spectral weighted radiative fluxes and radiative fluxes

---
+++ Data/integration_rf.R
@@ -0,0 +1,28 @@
+
+# This script calculates the radiant flux for each lamp output and the weighted radiant flux for each pair (lamp, weighting function)
+# doing the integral by wavelength
+
+# Loading data by executing AllData.R script
+source("Data/AllData.R")
+
+
+# Calculating the integral of radiant flux for each lamp
+integration_radiant_flux <- weighted_responses_df %>%
+  dplyr::select("Wavelength [nm]", lamp_name, wf_name, actual_radiant_flux) %>%
+  dplyr::filter(!is.na(actual_radiant_flux)) %>%
+  dplyr::distinct(dplyr::across(c(lamp_name, "Wavelength [nm]")), .keep_all = TRUE) %>%
+  dplyr::group_by(lamp_name) %>%
+  dplyr::summarise(
+    integrated_rf = MESS::auc(x = `Wavelength [nm]`, y = actual_radiant_flux)
+  )
+
+
+# Calculating the integral of weighted radiant flux for each couple (lamp, weighting function)
+integration_weighted_radiant_flux <- weighted_responses_df %>%
+  dplyr::select("Wavelength [nm]", lamp_name, wf_name, actual_radiant_flux, actual_weighted_radiant_flux) %>%
+  dplyr::filter(!is.na(actual_weighted_radiant_flux)) %>%
+  dplyr::group_by(lamp_name, wf_name) %>%
+  dplyr::summarise(
+    integrated_wrf = MESS::auc(x = `Wavelength [nm]`, y = actual_weighted_radiant_flux) * 683,
+    integrated_rf = MESS::auc(x = `Wavelength [nm]`, y = actual_radiant_flux)
+  )
