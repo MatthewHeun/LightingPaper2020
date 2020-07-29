eebfa82ba06ebab17e2196832b17b6b9d598cb75
ZekeMarshall <60969328+ZekeMarshall@users.noreply.github.com>
2020-07-29 15:20:44 +0100

    Alter code for lamp and wf lists to be dependent on the lamps and wf's selected in the rnw file

--- pieces/results_phi_xtable.R
+++ pieces/results_phi_xtable.R
@@ -1,16 +1,16 @@
 # Loading data by executing spd_metric_calcs.R script
 source("Data/spd_metric_calcs.R")
 
-# Creates a list of lamps for reporting of results
-lamp_list_for_phi <- c("lamp_inc_globe_a19_40w", "lamp_hps_phil", "lamp_cfl_es_tw_9", "lamp_led_phil_a19")
+# Creates a list of lamps for reporting of results based on the original data loaded in the .rnw file.
+lamp_list_for_results <-  paste("lamp_", lamp_list, sep = "")
 
-# Creates a list of weighting functions for reporting of results
-wf_list_for_phi <- c("wf_nw", "wf_v", "wf_p2", "wf_u")
+# Creates a list of weighting functions for reporting of results based on the original data loaded in the .rnw file.
+wf_list_for_results <- paste("wf_", wf_list, sep = "")
 
 # Filtering spd_metrics data frame to extract phi data.
 results_phi <- spd_metrics %>%
   dplyr::select(lamp_name, wf_name, phi_agg) %>%
-  dplyr::filter(lamp_name %in% lamp_list_for_etas & wf_name %in% wf_list_for_etas)
+  dplyr::filter(lamp_name %in% lamp_list_for_results & wf_name %in% wf_list_for_results)
 
 # Uses xtable function to generate table
 
