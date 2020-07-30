2280d262d83618bfe60c5e784f5ddd40a4217a17
ZekeMarshall <60969328+ZekeMarshall@users.noreply.github.com>
2020-07-30 11:13:12 +0100

    Added lamp info xtable chunk, and associated code.

---
+++ Data/results_lamp_info.R
@@ -0,0 +1,26 @@
+# This script formats the lamp info data into a table for rnw file.
+
+# Creates a list of lamps for reporting of results based on the original data loaded in the .rnw file.
+# These lamps differ to the lamp data used for the analysis, 
+# some of which are rescaled, and so have different codes and lamp info data.
+lamp_list_for_info <-  c("lamp_inc_globe_a19_40w",
+                            "lamp_hps_phil",
+                            "lamp_cfl_es_tw_9",
+                            "lamp_led_phil_a19")
+
+# Filters and selects the lamps in lamp_info. 
+results_lamp_info <- lamp_info %>%
+  dplyr::filter(lamp_name %in% lamp_list_for_info) %>%
+  dplyr::select(lamp_name, `Lamp Type`, Description, Year, 
+                `Luminous Efficacy [lm/W]`, 
+                `Electricity Consumption [W]`, 
+                `Luminous Efficiency [%]`)
+
+# Re-arranges the rows into the correct order
+results_lamp_info <- results_lamp_info[c(1,4,2,3),]
+
+# Removes lamp info column after ordering
+results_lamp_info <- results_lamp_info[,-1]
+
+
+
