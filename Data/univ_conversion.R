e1b6643f651bacf41b680cea9d2b6bab6177d0f9
ZekeMarshall <60969328+ZekeMarshall@users.noreply.github.com>
2020-08-06 17:38:24 +0100

    Added mean and sd for universal conversion factor across all lamps and lamp technologies.

--- Data/univ_conversion.R
+++ Data/univ_conversion.R
@@ -23,27 +23,18 @@ lamptype_info <- lamp_info %>%
 conversion_eta_df <- dplyr::full_join(conversion_eta_df, lamptype_info, by = "lamp_name")
 
 conversion_eta_df <- conversion_eta_df %>%
+  dplyr::filter(!is.na(eta_u)) %>%
+  dplyr::mutate(
+    mean_conv = mean(conv_eta_univ),
+    sd_conv = sd(conv_eta_univ)
+  )
+
+conversion_eta_df <- conversion_eta_df %>%
   dplyr::group_by(`Lamp type`) %>%
   dplyr::filter(!is.na(eta_u)) %>%
   dplyr::mutate(
-    mean_conv = mean(conv_eta_univ)
+    mean_lamp_conv = mean(conv_eta_univ),
+    sd_lamp_conv = sd(conv_eta_univ)
   )
 
-
-# Phi photopic to universal conversion factor
-# We don't need to ue this if we just use phi_u instead of phi_pl in the agg calcs
-
-univ_phi_df <- spd_metrics %>%
-  dplyr::filter(wf_name == "wf_u") %>%
-  dplyr::select(lamp_name, phi_agg)
-
-p2_phi_df <- spd_metrics %>%
-  dplyr::filter(wf_name == "wf_p2") %>%
-  dplyr::select(lamp_name, phi_agg)
-
-conversion_phi_df <- dplyr::full_join(univ_phi_df, p2_phi_df, by = "lamp_name")
-
-colnames(conversion_phi_df) <- c("lamp_name", "phi_u","phi_p2")
-
-conversion_phi_df <- conversion_phi_df %>%
-  dplyr::mutate(conv_phi_univ = phi_u / phi_p2)
+# .groups = "drop"
