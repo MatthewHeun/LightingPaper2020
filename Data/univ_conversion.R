c498df2cd8c0e9e09615a9e9bf82a9f8de3b2211
ZekeMarshall <60969328+ZekeMarshall@users.noreply.github.com>
2020-08-06 14:45:48 +0100

    Added code to calculate the conversion factor when moving from an FU eta based on the photopic luminosity function to an FU eta based on the universal luminosity function. Next step is to add more lamps then calculate the mean and sd over all lamps and within lamp technologies.

---
+++ Data/univ_conversion.R
@@ -0,0 +1,36 @@
+source("Data/spd_metric_calcs.R")
+
+# Eta photopic to universal conversion factor 
+
+univ_eta_df <- spd_metrics %>%
+  dplyr::filter(wf_name == "wf_u") %>%
+    dplyr::select(lamp_name, wrp_div_ec)
+
+p2_eta_df <- spd_metrics %>%
+  dplyr::filter(wf_name == "wf_p2") %>%
+  dplyr::select(lamp_name, wrp_div_ec)
+
+conversion_eta_df <- dplyr::full_join(univ_eta_df, p2_eta_df, by = "lamp_name")
+
+colnames(conversion_eta_df) <- c("lamp_name", "eta_u","eta_p2")
+
+conversion_eta_df <- conversion_eta_df %>%
+  dplyr::mutate(conv_eta_univ = eta_u / eta_p2)
+
+# Phi photopic to universal conversion factor
+# We don't need to ue this if we just use phi_u instead of phi_pl in the agg calcs
+
+univ_phi_df <- spd_metrics %>%
+  dplyr::filter(wf_name == "wf_u") %>%
+  dplyr::select(lamp_name, phi_agg)
+
+p2_phi_df <- spd_metrics %>%
+  dplyr::filter(wf_name == "wf_p2") %>%
+  dplyr::select(lamp_name, phi_agg)
+
+conversion_phi_df <- dplyr::full_join(univ_phi_df, p2_phi_df, by = "lamp_name")
+
+colnames(conversion_phi_df) <- c("lamp_name", "phi_u","phi_p2")
+
+conversion_phi_df <- conversion_phi_df %>%
+  dplyr::mutate(conv_phi_univ = phi_u / phi_p2)
