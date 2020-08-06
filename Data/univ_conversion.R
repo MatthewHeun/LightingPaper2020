6d6b445a06d230561c093e4e0600cf142bb7f8f0
ZekeMarshall <60969328+ZekeMarshall@users.noreply.github.com>
2020-08-06 16:38:42 +0100

    Updated code to average conversion factors across lamp types.

--- Data/univ_conversion.R
+++ Data/univ_conversion.R
@@ -17,6 +17,19 @@ colnames(conversion_eta_df) <- c("lamp_name", "eta_u","eta_p2")
 conversion_eta_df <- conversion_eta_df %>%
   dplyr::mutate(conv_eta_univ = eta_u / eta_p2)
 
+lamptype_info <- lamp_info %>%
+  dplyr::select(lamp_name, `Lamp type`)
+
+conversion_eta_df <- dplyr::full_join(conversion_eta_df, lamptype_info, by = "lamp_name")
+
+conversion_eta_df <- conversion_eta_df %>%
+  dplyr::group_by(`Lamp type`) %>%
+  dplyr::filter(!is.na(eta_u)) %>%
+  dplyr::mutate(
+    mean_conv = mean(conv_eta_univ)
+  )
+
+
 # Phi photopic to universal conversion factor
 # We don't need to ue this if we just use phi_u instead of phi_pl in the agg calcs
 
