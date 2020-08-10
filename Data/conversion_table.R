c79937f357953f98aa1c4eb55b4c7db0e1f393f0
ZekeMarshall <60969328+ZekeMarshall@users.noreply.github.com>
2020-08-10 14:40:43 +0100

    Deleted comments that have been resolved. Changed rownames, and number of decimal places in conversion table.

--- Data/conversion_table.R
+++ Data/conversion_table.R
@@ -8,11 +8,11 @@ conversion_table <- lamps_in_paper_stats_X
 info_ct_colnames <- conversion_table$lamp_type
 
 # Sets the number of decimal places for each column
-conversion_table[, "count"] <- round(conversion_table[, "count"],0)
+# conversion_table[, "count"] <- round(conversion_table[, "count"],0)
 
-conversion_table[, "mean_conv"] <- round(conversion_table[, "mean_conv"],2)
+# conversion_table[, "mean_conv"] <- round(conversion_table[, "mean_conv"],2)
 
-conversion_table[, "sd_conv"] <- round(conversion_table[, "sd_conv"],2)
+# conversion_table[, "sd_conv"] <- round(conversion_table[, "sd_conv"],2)
 
 # Transposes the combined DF
 conversion_table <- as.data.frame(t(conversion_table[,-1]))
@@ -24,11 +24,11 @@ colnames(conversion_table) <- info_ct_colnames
 conversion_table <- conversion_table[,c(3,2,1,4)]
 
 # Re-orders rows
-conversion_table <- conversion_table[c(2,3,1), ]
+conversion_table <- conversion_table[c("mean_conv","sd_conv","count"), ]
 
 # Set rownames
 
 conversion_table <- magrittr::set_rownames(conversion_table, 
-                                            c("Sample size", 
-                                              "Mean conversion factor ($\\gammaratavg{}$)", 
-                                              "Standard deviation ($\\sigma_{\\gammarat}$)"))  
+                                            c("Mean conversion factor ($\\gammaratavg{}$)", 
+                                              "Standard deviation ($\\sigma_{\\gammarat}$)",
+                                              "Sample size"))  
