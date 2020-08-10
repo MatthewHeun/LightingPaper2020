cc861c6e61d8242ceab2f0fc4a7ccc17b01c32d2
ZekeMarshall <60969328+ZekeMarshall@users.noreply.github.com>
2020-08-10 14:06:59 +0100

    Manually set the number of DP in the conversion table, changed the order of the rows.

--- Data/conversion_table.R
+++ Data/conversion_table.R
@@ -7,6 +7,13 @@ conversion_table <- lamps_in_paper_stats_X
 # Identifies column names 
 info_ct_colnames <- conversion_table$lamp_type
 
+# Sets the number of decimal places for each column
+conversion_table[, "count"] <- round(conversion_table[, "count"],0)
+
+conversion_table[, "mean_conv"] <- round(conversion_table[, "mean_conv"],2)
+
+conversion_table[, "sd_conv"] <- round(conversion_table[, "sd_conv"],2)
+
 # Transposes the combined DF
 conversion_table <- as.data.frame(t(conversion_table[,-1]))
 
@@ -15,6 +22,9 @@ colnames(conversion_table) <- info_ct_colnames
 
 # Re-orders columns
 conversion_table <- conversion_table[,c(3,2,1,4)]
+
+# Re-orders rows
+conversion_table <- conversion_table[c(2,3,1), ]
 
 # Set rownames
 
