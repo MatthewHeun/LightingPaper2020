35c401eaaf6b8bea0bb732687e16b796bba5a3b6
ZekeMarshall <60969328+ZekeMarshall@users.noreply.github.com>
2020-08-20 07:56:34 +0100

    Deleted redundant script file.

--- Submissions/Submission 1/Supplementary Materials/Data to prepare for SM/sea_lighting_efficiency.R
+++
@@ -1,4 +0,0 @@
-# This function reads the SEA lighting efficiency data in pre-prepared in panel data format.
-library(readxl)
-sea_le_df <- SEA_lighting_efficiency <- read_excel("Data/SEA_lighting_efficiency.xlsx", 
-                                      sheet = "SEA_le_panel")
